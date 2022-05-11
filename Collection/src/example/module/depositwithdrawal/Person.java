80e73e6781a5f66efa60b9573e1085d65b9bbbdb
nowonjae <nodnjs1231@smartix.co.kr>
2022-05-11 17:02:25 +0900

    별도 패키지 Main.class Run

---
+++ Collection/src/example/module/depositwithdrawal/Person.java
@@ -0,0 +1,8 @@
+package example.module.depositwithdrawal;
+
+public class Person {
+    String name;
+    int age;
+    int cashAmount;
+    BankAccount account;
+}
---
+++ Collection/src/runtest/pack/print/Main.java
@@ -0,0 +1,7 @@
+package runtest.pack.print;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("different package Main class start ok");
+    }
+}
